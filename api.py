863aa49c82d895c8c63f292145f400b83b8056a5
matsutani <matsutani@n2sm.net>
2017-01-22 21:30:04 +0900

    socl change

--- api.py
+++ api.py
@@ -23,7 +23,7 @@ CORS(app)
 
 @app.route('/')
 def index():
-    return "使い方 : /api/検索する単語/取得ページ数"
+    return "使い方 : /api/ページ数/アーティスト名/セクション"
 
 @app.route('/api/<int:page>/<string:artist>/<string:sect>') # ページ数/アーティスト/検索場所をパスから変数に受け取る
 def sound(page,artist,sect):
@@ -36,60 +36,10 @@ def sound(page,artist,sect):
 
     URL = "https://soundcloud.com/"+str(artist)+"/"+str(sect)
     driver.get(URL) # slideshareのURLにアクセスする
-    #driver.get(setparam.get('settings', 'URL')) # slideshareのURLにアクセスする
+    #driver.get(setparam.get('settings', 'URL')+str(artist)+str(sect)) # slideshareのURLにアクセスする
     
     data_list = [] # 全ページのデータを集める配列
     data_list = (bosc.bottomScroll(driver, page))
-
-    #search = driver.find_element_by_id(setparam.get('settings', 'search_box_element')) # 検索欄要素を取得
-    ##search = driver.find_element_by_id("nav-search-query") # 検索欄要素を取得
-    #search.send_keys(artist) # 検索ワードを入力
-    #search.submit() # 検索をsubmitする
-
-    #lang = driver.find_element_by_xpath("//select[@id='slideshows_lang']/option[@value='ja']") # 言語選択リストの日本語の部分を抽出
-    #lang.click() # 言語選択の日本語を選択
-
-#    for i in range(0,page): 
-#        print(str(i+1) + u"ページ目")
-#        data = driver.page_source.encode('utf-8') # ページ内の情報をutf-8で用意する
-#        soup = BeautifulSoup(data,"lxml") # 加工しやすいようにlxml形式にする
-#        #slide_list = soup.find_all("div",class_="thumbnail-content") # スライド単位で抽出
-#        sound_list = soup.find_all("li",class_="soundList__item") # スライド単位で抽出
-#        for sound in sound_list:
-#            sound_in = {} # スライドの情報を辞書形式でまとめる
-#            
-#            # スライドの投稿者の名前を入手
-#            name = sound.find("div",class_="sc-type-light soundTitle__secondary").text
-#            slide_in["name"] = name.strip() # strip()は両端の空白と改行をなくしてくれる
-#            
-#            # スライドのタイトルを入手
-#            title = slide.find("a",class_="title title-link antialiased j-slideshow-title").get("title") # 指定したタグ&クラス内のtitleを出す
-#            slide_in["title"] = title
-#
-#            # スライドのリンクを入手
-#            link = slide.find("a",class_="title title-link antialiased j-slideshow-title").get("href") # 指定したタグ&クラス内のhrefを出す
-#            slide_in["link"] = "http://www.slideshare.net" + link
-#            
-#            # スライドのサムネのリンクを入手
-#            imagetag = slide.find("a",class_="link-bg-img").get("style") # 指定したタグ&クラス内のstyleを出す
-#            image = imagetag[imagetag.find("url(")+4:imagetag.find(");")] # いらない部分を取り除く
-#            slide_in["image"] = image
-#            
-#            # スライドのページ数であるslidesとlikesを入手
-#            info = slide.find("div",class_="small-info").string # slidesとlikesの文字列を入手
-#            slides = info[7:info.find("slides")] # slides部分を抽出
-#            slide_in["slides"] = slides.strip() # strip()は両端の空白と改行をなくしてくれる
-#            if "likes" in info:
-#                likes = info[info.find(", ")+2:info.find("likes")] # likes部分を抽出
-#            else:
-#                likes = "0"
-#            slide_in["likes"] = likes.strip() # strip()は両端の空白と改行をなくしてくれる
-#
-#            data_list.append(slide_in) # data_listに1ページ分の内容をまとめる
-#
-#        driver.execute_script('window.scrollTo(0, document.body.scrollHeight)') # ページャーのある下に移動
-#        next = driver.find_element_by_xpath("//li[@class='arrow']/a[@rel='next']") # ページャーのNEXT要素を抽出
-#        next.click() # Nextボタンをクリック
 
     driver.close() # ブラウザ操作を終わらせる
     jsonstring = json.dumps(data_list,ensure_ascii=False,indent=int(setparam.get('settings', 'json_indent'))) # 作った配列をjson形式にして出力する
--- bottomScroll.py
+++ bottomScroll.py
@@ -1,19 +1,66 @@
 # -*- coding: utf-8 -*-
+
+# ここからスクレイピング必要分
+from bs4 import BeautifulSoup
+# ここからseleniumでブラウザ操作必要分
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys # 文字を入力する時に使う
 from time import sleep
 
 def bottomScroll(driver, scrollpage):
+	data_list = [] # 全ページのデータを集める配列
 	if isinstance( scrollpage,int ):
 		if 0 == scrollpage:
 			lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 			match=False
 			while(match==False):
 				lastCount = lenOfPage
-				sleep(2)
+				sleep(3)
 				lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 				if lastCount==lenOfPage:
 					match=True
-		elif 0 < scrollpage: 
-			for i in range(scrollpage):
-				driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-				sleep(2)
+            
+			data = driver.page_source.encode('utf-8')
+			soup = BeautifulSoup(data,"lxml") # 加工しやすいようにlxml形式にする
+			sound_list = soup.find_all("li",class_="soundList__item") # サウンド単位で抽出
+			print(str(len(sound_list)) + u"件")
+			for sound in sound_list:
+				sound_in = {} # サウンドの情報を辞書形式でまとめる
+                
+				# サウンドの投稿者の名前を入手
+				name = sound.find("span",class_="soundTitle__usernameText").text
+				#print(str(name))
+				sound_in["name"] = name.strip() # strip()は両端の空白と改行をなくしてくれる
+                
+				# サウンドのタイトルを入手
+				title = sound.find("a",class_="soundTitle__title").find("span").text
+				#title = titles.find("span").text_content()
+				#print(str(title).encode('utf-8'))
+				# 指定したタグ&クラス内のtitleを出す
+				sound_in["title"] = title.strip()
+                
+				# サウンドのリンクを入手
+				link = sound.find("a",class_="soundTitle__title").get("href")
+				# 指定したタグ&クラス内のhrefを出す
+				#print(str(link))
+				sound_in["link"] = "https://soundcloud.com" + link
+                
+				# 投稿日時
+				uploadTime = sound.find("time").get("datetime")
+				#print(str(uploadTime))
+				sound_in["uploadTime"] = uploadTime.strip()
+                
+				# 投稿経過時間
+				postedTime = sound.find("time").get("title")
+				#print(str(postedTime))
+				sound_in["postedTime"] = postedTime.strip()
+                
+				# 再生回数
+				plays = sound.find("li", class_="sc-ministats-item").get("title")
+				#print(str(plays))
+				sound_in["plays"] = plays
+                
+				data_list.append(sound_in) # data_listに1ページ分の内容をまとめる
+
+			return data_list
+
